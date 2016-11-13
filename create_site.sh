6ed14fabc81d0f089d90b91077318e6cfba28abd
Alvaro Mercado <alvaro@debian.alvaropc>
2016-11-13 18:17:53 -0300

    create_site index created

--- create_site.sh
+++ create_site.sh
@@ -1,18 +1,25 @@
-#!/bin/sh
+#/bin/sh
 # @autor alvar01omer@gmail.com 
 
 _GITS_HOLE="GIT_SOURCE/"
-_SERVER_DIRECTION="162.243.7.240"
-_SERVER_PATH="/var/www/html/"
+_SERVER_DIRECTION="127.0.0.1"
+_SERVER_PATH="/opt/lampp/htdocs/"
+
 #cd to home
 cd ~
+
 #create repo
 git init --bare $_GITS_HOLE${1}.git
 #cd in repo
 cd $_GITS_HOLE${1}.git
-#rutine after push
-echo "#!/bin/sh \n GIT_WORK_TREE=${_SERVER_PATH}${1} git checkout -f" > hooks/post-receive
-#permissions changed
+
+if [ $2=="index" ] 
+then
+	echo "#!/bin/sh \n GIT_WORK_TREE=${_SERVER_PATH} git checkout -f" > hooks/post-receive
+else 
+	echo "#!/bin/sh \n GIT_WORK_TREE=${_SERVER_PATH}${1} git checkout -f" > hooks/post-receive
+fi
+
 chmod +x hooks/post-receive
 
-echo "git remote add $1  ssh://${USER}@${_SERVER_DIRECTION}/home/${USER}/${_GITS_HOLE}${1}.git && git push --set-upstream ${1} master TO LOCAL REPO"
+echo "git remote add $1  ssh://${USER}@${_SERVER_DIRECTION}/home/${USER}/${_GITS_HOLE}${1}.git && git push --set-upstream ${1} master   TO LOCAL REPO"
