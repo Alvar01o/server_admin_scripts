aa8e9ccb41a5ca195e672e8aa621737b7ec4d9c5
Alvaro Mercado <alvar01omer@gmal.com>
2016-11-14 01:22:14 +0000

    create_site changes

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ File: aliases.sh
 > load this file in .bashrc : 
 > source ~/server_admin_scripts/aliases.sh
 
-Command: create_site
+Command: create_site <sitename>
 > prepare the git server and put source in _SERVER_PATH
+	<sitename> repo name and folder in _SERVER_PATH to sync code
+### OPTIONS
+	index: sync project source in _SERVER_PATH
--- create_site.sh
+++ create_site.sh
@@ -13,12 +13,14 @@ git init --bare $_GITS_HOLE${1}.git
 #cd in repo
 cd $_GITS_HOLE${1}.git
 
-if [ $2 == "index" ] 
-then
-	echo $2
+if [ "$2" == "index" ]; then
+	echo "Work dir seted on : ${_SERVER_PATH}"
 	echo -e "#!/bin/sh \n GIT_WORK_TREE=${_SERVER_PATH} git checkout -f" > hooks/post-receive
 else 
+	mkdir ${_SERVER_PATH}${1}
 	echo -e "#!/bin/sh \n GIT_WORK_TREE=${_SERVER_PATH}${1} git checkout -f" > hooks/post-receive
+        echo "Work dir seed on : ${_SERVER_PATH}${1}"
+
 fi
 
 chmod +x hooks/post-receive
