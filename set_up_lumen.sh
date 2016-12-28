0bd073beb84993ce8c8d57fe3021db81521faf11
root <root@debian.alvaropc>
2016-12-28 02:12:42 -0300

    lumen installer for server

--- set_up_lumen.sh
+++ set_up_lumen.sh
@@ -1,3 +1,20 @@
-./tools/installer_composer
+#!/bin/bash
 
-mv tools/composer.phar /usr/local/bin/composer
+_COMPOSER_DIR='tools/composer.phar'
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#check if exist composer file
+if [ ! -f ${_COMPOSER_DIR} ]; 
+then
+	php tools/installer_composer
+fi
+
+#cp file with permissions
+su - -c "cp ${DIR}/${_COMPOSER_DIR} /usr/local/bin/composer"
+
+#cd to home
+cd ~
+
+#install lumen
+composer global require "laravel/lumen-installer"
